7c57c0da45f1dacee868ff77f83d3a6d24b1e767
Mukaribatul-cha <139300230+Mukaribatul-cha@users.noreply.github.com>
2025-05-09 14:44:11 +0700

    caca

---
+++ template/navbar.php
@@ -0,0 +1,57 @@
+<body class="sb-nav-fixed">
+        <nav class="sb-topnav navbar navbar-expand navbar-dark bg-primary">
+            <!-- Navbar Brand-->
+            <a class="navbar-brand ps-3" href="<?= $main_url ?>index.php">Metode Smart</a>
+            <!-- Sidebar Toggle-->
+            <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
+            <!-- Navbar Search-->
+            <form class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
+                <span class="text-white text-capitalize"><?= $_SESSION["ssUser"]?></span>
+            </form>
+            <!-- Navbar-->
+            <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
+                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" data-bs-toggle="modal" href="#mdlProfilUser">Profile User</a></li>
+                        <li><a class="dropdown-item" href="<?= $main_url ?>auth/logout.php">Logout</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+
+        <?php
+            $username = $_SESSION["ssUser"];
+        
+           
+        ?>
+
+        <div class="modal" tabindex="-1" id="mdlProfilUser">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Profil User</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                <div class="card mb-3" style="max-width: 540px;">
+                    <div class="row g-0">
+                        <div class="col-md-8">
+                        <div class="card-body">
+                        <h4 class="card-title mb-3 text-capitalize ps-1"><?= $username ?></h4>
+                        <hr>
+                        <div class=" row">
+                            <label for="nama" class="col-sm-3 col-form-label">Nama</label>
+                            <div class="col-sm-9">
+                            <input type="text" readonly class="form-contro border-0 bg-transparentl" id="nama"  value=": <?= $username ?>" readonly>
+                            </div>
+                          
+                        </div>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+                </div>
+                </div>
+            </div>
+        </div>
---
+++ template/sidbarr.php
@@ -0,0 +1,53 @@
+<div id="layoutSidenav">
+            <div id="layoutSidenav_nav">
+                <nav class="sb-sidenav accordion bg-light" id="sidenavAccordion">
+                    <div class="sb-sidenav-menu">
+                        <div class="nav">
+                            <div class="sb-sidenav-menu-heading">Al Asror</div>
+                            <a class="nav-link" href="<?= $main_url ?>index.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                                Dashboard
+                            </a>
+                            <hr class="ab-0">
+                            <a class="nav-link" href="<?= $main_url ?>user/alternatif.php">
+                                <div class="sb-nav-link-icon"><i class="fa-solid fa-network-wired"></i></i></div>
+                                Data Alternatif
+                            </a>
+                            <a class="nav-link" href="<?= $main_url ?>user/kriteria.php">
+                                <div class="sb-nav-link-icon"><i class="fa-solid fa-address-book"></i></i></div>
+                                Data Kriteria
+                            </a>
+                            <a class="nav-link" href="<?= $main_url ?>user/subkriteria.php">
+                                <div class="sb-nav-link-icon"><i class="fa-brands fa-readme"></i></div>
+                                Data Subkriteria
+                            </a>
+                            <a class="nav-link" href="<?= $main_url ?>user/penilaian.php">
+                                <div class="sb-nav-link-icon"><i class="fa-solid fa-marker"></i></div>
+                                Data Penilaian
+                            </a>
+                            <a class="nav-link" href="<?= $main_url ?>user/perhitungan.php">
+                                <div class="sb-nav-link-icon"><i class="fa-solid fa-calculator"></i></div>
+                                Data Perhitungan
+                            </a>
+                            <a class="nav-link" href="<?= $main_url ?>user/hasilakhir.php">
+                                <div class="sb-nav-link-icon"><i class="fa-solid fa-lightbulb"></i></div>
+                                Data Hasil Akhir
+                            </a>
+                            <hr class="ab-0">
+                            <div class="sb-sidenav-menu-heading">Admin</div>
+                            <a class="nav-link" href="<?= $main_url ?>user/add-user.php">
+                                <div class="sb-nav-link-icon"><i class="fa-solid fa-user"></i></i></div>
+                                User
+                            </a>
+                            <a class="nav-link" href="<?= $main_url ?>user/password.php">
+                                <div class="sb-nav-link-icon"><i class="fa-solid fa-lock"></i></i></div>
+                                Ganti Password
+                            </a>
+                        </div>
+                    </div>
+                    <div class="sb-sidenav-footer">
+                        <div class="small">Logged in as:</div>
+                        <?= $_SESSION["ssUser"] ?>
+                    </div>
+                </nav>
+            </div>
